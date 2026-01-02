4f46323553792296ca5c2ee3db23c5acfa854d8c
Soubhik Halder <soubhikhalder82@gmail.com>
2026-01-02 18:37:35 +0530

    user and video models added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import 'dotenv/config';
 import connectDB from "./db/index.js";
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at port:${process.env.PORT}`)
+    })
+    app.on("error",(err)=>{
+        console.log(err);
+        process.exit(1);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+})
 
 
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+
+// this is the old way of handling errors
+// const asyncHandler = (fn) => async(req,res,next) =>{
+//     try {
+//         await fn(req,res,next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message,
+//         })
+//     }
+// }
+// this is the new way of handling errors
+const asyncHandler = (requestHandler) => {
+    return (req,res,next) =>{
+    Promise.resolve(requestHandler(req,res,next)).catch(error => next(error)); 
+    }
+}
+
+export {asyncHandler} 
